5a8c507b9de828bd492253885af97a84d23f5c24
Alan <epvelod@hotmail.com>
2019-11-10 15:28:00 -0600

    idUser

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import InicioNavegacion from './Componentes/InicioNavegacion.js';
 
 export default class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      userId: undefined,
+    };
+  };
+  async componentDidMount() {
+    const itemUsuario = await AsyncStorage.getItem('myuser')||"{}";
+    const myuser = JSON.parse(itemUsuario);
+    this.setState({
+      ...this.state,
+      userId: myuser.userId
+    });
+  }  
   render(){
     return(
-      <InicioNavegacion />
+      <InicioNavegacion screenProps={{idUser: this.state.userId}} />
     )
   }
 }
